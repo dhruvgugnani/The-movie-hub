6e301fc293cc506a457b0ef54c150838150e4633
dhruvgugnani <gugnanidhruv@gmail.com>
2026-03-06 11:42:41 +0530

    removed vite proxy

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),], server: {
-    proxy: {
-      "/tmdb": {
-        target: "https://api.themoviedb.org/3",
-        changeOrigin: true,
-        rewrite: (path) => path.replace("/tmdb", ""),
-      },
-    },
   },
 
 
